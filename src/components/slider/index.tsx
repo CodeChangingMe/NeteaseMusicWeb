962dcbe5b442ea255ad678491b542db79561b20e
吴磊 <1036285587@qq.com>
2020-01-17 20:14:39 +0800

    推荐页轮播图

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
+import { SliderContainer } from './style';
+
+interface SliderProps {
+  bannerList: { imgUrl: string }[];
+}
+
+export function Slider(props: SliderProps) {
+  const [sliderSwiper, setSliderSwiper] = useState<Swiper | null>(null);
+  const { bannerList } = props;
+
+  useEffect(() => {
+    // 只有在有图片的时候，才会去做初始化
+    if (bannerList.length && !sliderSwiper) {
+      let sliderSwiper = new Swiper('.slider-container', {
+        speed: 1000,
+        loop: true,
+        autoplay: true,
+        pagination: { el: '.swiper-pagination' }
+      });
+      setSliderSwiper(sliderSwiper);
+    }
+  }, [bannerList, sliderSwiper]);
+
+  return (
+    <SliderContainer>
+      <div className="before"></div>
+      <div className="slider-container">
+        <div className="swiper-wrapper">
+          {bannerList.map((slider, index) => {
+            return (
+              <div className="swiper-slide" key={slider.imgUrl + index}>
+                <div className="slider-nav">
+                  <img
+                    src={slider.imgUrl}
+                    width="100%"
+                    height="100%"
+                    alt="推荐"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    </SliderContainer>
+  );
+}
+
+export default React.memo(Slider);
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
+// 开启Redux DevTools
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
